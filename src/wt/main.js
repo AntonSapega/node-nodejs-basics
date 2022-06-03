3c1a8d991310e01393ce2e1a329c15e2118f27f2
Anton Sapeha <antonsapega@gmail.com>
2022-06-04 02:26:44 +0500

    worker threads task is completed

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,3 +1,50 @@
+import { cpus } from "os";
+import { Worker } from "worker_threads";
+
 export const performCalculations = async () => {
-    // Write your code here
+  const coresAmount = cpus().length;
+  const startNumber = 10;
+  let results = null;
+  const workers = openWorkers(startNumber, coresAmount + startNumber);
+
+  await Promise.allSettled(workers).then((arr) => {
+    results = handleWorkerResponse(arr);
+  });
+  return results;
 };
+
+function openWorkers(initialValue, limit) {
+  const workers = [];
+
+  for (let i = initialValue; i < limit; i++) {
+    const worker = new Worker("./worker.js", { workerData: i });
+    const workerPromise = new Promise((resolve, reject) => {
+      worker.on("message", (msg) => {
+        resolve(msg);
+      });
+      worker.on("error", (err) => reject(err));
+    });
+
+    workers.push(workerPromise);
+  }
+
+  return workers;
+}
+
+function handleWorkerResponse(dataArr) {
+  return dataArr.map((obj) => {
+    if (obj.status === "fulfilled") {
+      return {
+        status: "resolved",
+        data: obj.value,
+      };
+    } else {
+      return {
+        status: "error",
+        data: null,
+      };
+    }
+  });
+}
+
+console.log(await performCalculations());
--- src/wt/worker.js
+++ src/wt/worker.js
@@ -1,6 +1,18 @@
+import { workerData, parentPort } from "worker_threads";
+
 // n should be received from main thread
-export const nthFibonacci = (n) => n < 2 ? n : nthFibonacci(n - 1) + nthFibonacci(n - 2);
+export const nthFibonacci = (n) =>
+  n < 2 ? n : nthFibonacci(n - 1) + nthFibonacci(n - 2);
 
 export const sendResult = () => {
-    // This function sends result of nthFibonacci computations to main thread
+  // This function sends result of nthFibonacci computations to main thread
+  const result = nthFibonacci(workerData);
+  //! Just for an example!!!
+  if (workerData === 11) {
+    throw new Error("Something went wrong");
+  } else {
+    parentPort.postMessage(result);
+  }
 };
+
+sendResult();
