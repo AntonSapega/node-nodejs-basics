6b1a821d3bc5ede647a8053dff1c4051d4cc6f7d
Anton Sapeha <antonsapega@gmail.com>
2022-06-03 17:19:46 +0500

    zlib task is completed

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,3 +1,12 @@
+import { createReadStream, createWriteStream } from "fs";
+import zlib from "zlib";
+
 export const compress = async () => {
-    // Write your code here 
+  const readStream = createReadStream("./files/fileToCompress.txt", "utf8");
+  const writeStream = createWriteStream("./files/archive.gz");
+  const compressStream = zlib.createGzip();
+
+  readStream.pipe(compressStream).pipe(writeStream);
 };
+
+compress();
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,3 +1,12 @@
+import zlib from "node:zlib";
+import { createReadStream, createWriteStream } from "node:fs";
+
 export const decompress = async () => {
-    // Write your code here 
+  const readStream = createReadStream("./files/archive.gz");
+  const decompress = zlib.createUnzip();
+  const writeStream = createWriteStream("./files/fileToCompress.txt");
+
+  readStream.pipe(decompress).pipe(writeStream);
 };
+
+decompress();
